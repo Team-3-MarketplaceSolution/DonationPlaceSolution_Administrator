04de5bee165f99ee66229e87ef561d7fb96cef6a
Claire <claire.cy.t@gmail.com>
2020-04-23 02:34:36 -0400

    Only allow administrator account sign in

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import {View, Text, ActivityIndicator, StyleSheet} from 'react-native'
 import * as firebase from "firebase";
 
 export default class LoadingScreen extends React.Component {
+    state = {isAdmin: null}
+
     componentDidMount() {
+
         firebase.auth().onAuthStateChanged(user => {
+
+            if (user != null) {
+                firebase.database().ref('Admins/' + user.uid).once("value")
+                    .then( (snapshot) =>{
+                        this.setState({isAdmin: snapshot.exists()});
+                    });
+            } else {
+                this.setState({isAdmin: null})
+            }
+
+            console.log('isAdmin', this.state.isAdmin);
             let emailVerified;
             emailVerified=user?user.emailVerified: null;
-            this.props.navigation.navigate(emailVerified? "Main": "Auth")
-            user?console.log('Email Verified', user.emailVerified):null;
+            this.props.navigation.navigate((emailVerified && this.state.isAdmin && user) ? "Main" : "Auth")
+            user ? console.log('Email Verified', user.emailVerified) : null;
         })
     }
 
@@ -16,7 +30,7 @@ export default class LoadingScreen extends React.Component {
         return (
             <View style={styles.container}>
                 <Text>Loading</Text>
-                <ActivityIndicator size="large" />
+                <ActivityIndicator size="large"/>
             </View>
         )
     }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,47 +1,60 @@
 import React, {Component} from 'react';
-import { Image, View, Text, TextInput, TouchableOpacity,StyleSheet, Button} from 'react-native';
+import {Image, View, Text, TextInput, TouchableOpacity, StyleSheet, Button} from 'react-native';
 import StaticContainer from "react-native-web/src/vendor/react-native/StaticContainer";
 import * as firebase from "firebase";
 import Colors from "../constants/Colors";
 import SubmitButton from "../components/SubmitButton";
 
 
-
 //LOGIN CONTAINER
-export default class LoginScreen extends React.Component{
-    state = { email: '', password: '', errorMessage: null }
+export default class LoginScreen extends React.Component {
+    state = {email: '', password: '', errorMessage: null}
+
     handleLogin = () => {
+        let isAdmin;
         console.log(this.state.errorMessage);
         const {email, password} = this.state;
         firebase.auth().signInWithEmailAndPassword(email, password).catch(error => this.setState({errorMessage: error.message}));
         firebase.auth().onAuthStateChanged(user => {
-            if(!user.emailVerified){
-                // console.log(user.emailVerified);
-                // user.sendEmailVerification().then(function() {
-                //     alert('Please verify your email! New verification email send!');
-                // }).catch(function(error) {
-                //     // An error happened.
-                // });
-                alert('Please verify your email!');
-            }else{
-                this.props.navigation.navigate( "Main");
+
+            if (user != null) {
+                firebase.database().ref('Admins/' + user.uid).once("value")
+                    .then((snapshot) => {
+                        if(snapshot.exists()){
+                            if (!user.emailVerified) {
+                                // console.log(user.emailVerified);
+                                // user.sendEmailVerification().then(function() {
+                                //     alert('Please verify your email! New verification email send!');
+                                // }).catch(function(error) {
+                                //     // An error happened.
+                                // });
+                                alert('Please verify your email!');
+                            } else {
+                                this.props.navigation.navigate("Main");
+                            }
+                        }else{
+                            alert('Please log in with administrator account!');
+                        }
+                    });
             }
+
         })
 
 
-
     }
+
     render() {
         return (
             <View style={styles.container}>
-                <Image style={{padding:50}} source={require('../assets/images/D_logo.png')}/>
-                <Text style={{color: Colors.buttonColor, padding:20, fontSize: '20em',fontWeight:'bold'}}>Administrator Portal</Text>
-                <Text>{this.state.errorMessage}</Text>
+                <Image style={{padding: 50}} source={require('../assets/images/D_logo.png')}/>
+                <Text style={{color: Colors.buttonColor, padding: 20, fontSize: '20em', fontWeight: 'bold'}}>Administrator
+                    Portal</Text>
+                <Text styel ={{color:'red'}}>{this.state.errorMessage}</Text>
                 <TextInput
                     style={styles.textInput}
                     autoCapitalize="none"
                     placeholder="Email"
-                    onChangeText={email => this.setState({ email })}
+                    onChangeText={email => this.setState({email})}
                     value={this.state.email}
                 />
                 <TextInput
@@ -49,16 +62,16 @@ export default class LoginScreen extends React.Component{
                     style={styles.textInput}
                     autoCapitalize="none"
                     placeholder="Password"
-                    onChangeText={password => this.setState({ password })}
+                    onChangeText={password => this.setState({password})}
                     value={this.state.password}
                 />
-                <SubmitButton click = {this.handleLogin}>Sign In</SubmitButton>
+                <SubmitButton click={this.handleLogin}>Sign In</SubmitButton>
                 <View style={styles.signUpContainer}>
-                <Text >Don't have an account?</Text>
-                <Button
-                    title="Sign Up"
-                    onPress={() => this.props.navigation.navigate('SignUp')}
-                />
+                    <Text>Don't have an account?</Text>
+                    <Button
+                        title="Sign Up"
+                        onPress={() => this.props.navigation.navigate('SignUp')}
+                    />
                 </View>
             </View>
 
@@ -83,7 +96,7 @@ const styles = StyleSheet.create({
         color: "#161F3D",
         marginTop: 10
     },
-    signUpContainer:{
+    signUpContainer: {
         flexDirection: "row",
         alignItems: 'center',
         position: 'absolute',
